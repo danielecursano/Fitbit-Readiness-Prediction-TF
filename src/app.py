473ef156ebc00b92282ec3ae310049546746f89b
Daniele Cursano <daniele@Air-di-Daniele.lan>
2024-04-15 22:24:45 +0200

    web app interface

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from model import Model
+import numpy as np
+
+model = Model("/Users/daniele/Desktop/fitbit/data/fitbit-model.h5", (1, 11))
+app = Flask(__name__)
+
+@app.route("/")
+def index(output=None):
+    summary = []
+    model.model.summary(print_fn=lambda x: summary.append(x))
+    summary = "\n".join(summary)
+    return render_template("home.html", summary=summary, output=output)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    arr = np.array([float(request.form[f'float{i}']) for i in range(1, 12)]).reshape(model.input_shape)
+    return index(output=str(model.predict(arr)).replace("[", "").replace("]", ""))
+
+if __name__ == "__main__":
+    app.run(debug=1)
--- src/model.py
+++ src/model.py
@@ -14,6 +14,12 @@ class Model:
     def predict(self, vector):
         assert vector.shape == self.input_shape
         return self.model.predict(vector)
+    
+    def train(self, x_train, y_train, epochs=100, batch_size=32, validation_split=0.2):
+        self.model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
+    
+    def save(self, filepath):
+        self.model.save(filepath)
 
 if __name__ == "__main__":
     model = Model("/Users/daniele/Desktop/fitbit/data/fitbit-model.h5", (1, 11))
@@ -22,4 +28,6 @@ if __name__ == "__main__":
                     3.64100000e+00,9.61000000e+01, 7.80000000e+01,
                     2.10000000e+01, 9.30000000e+01,5.40000000e+01,
                     7.12008502e-02, 7.50000000e+01]).reshape((1, 11))
-    print(model.predict(inp))
+    out = model.predict(inp)
+    out = max(max(out, 0), min(out, 100))
+    print(out)
